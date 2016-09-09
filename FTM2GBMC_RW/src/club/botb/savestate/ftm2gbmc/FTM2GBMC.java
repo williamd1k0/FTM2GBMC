c19c5e427c2b3b7275ef81dcdfae1250c8ca0c02
Joseph El-Khouri <joseph.elkhouri@gmail.com>
2016-09-09 13:56:57 -0400

    adding more skeleton stuff

---
+++ FTM2GBMC_RW/src/club/botb/savestate/ftm2gbmc/FTM2GBMC.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package club.botb.savestate.ftm2gbmc;
+
+/**
+ *
+ * @author Savestate
+ */
+public class FTM2GBMC {
+    
+}
